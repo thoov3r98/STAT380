f6b334b16608a6b8c08ecab757f8d961498d9fee
thoov3r98 <90732183+thoov3r98@users.noreply.github.com>
2022-01-28 12:23:10 -0500

    Create nullscript.R

---
+++ Homework/HousePrices/project/src/models/nullscript.R
@@ -0,0 +1,16 @@
+trainingdata <- read.csv('Stat_380_train2022.csv')
+testingdata <- read.csv('Stat_380_test2022.csv')
+
+cor(trainingdata[sapply(trainingdata,is.numeric)])
+
+model = lm(SalePrice ~ OverallQual + YearBuilt + TotalBsmtSF + GrLivArea, data = trainingdata)
+
+summary(model)
+
+linear_prediction <- predict(model, testingdata)
+linear_prediction <- data.frame( Id = testingdata$Id, SalePrice = linear_prediction)
+
+#To verify it worked, look at the top rows of the two prediction data sets.
+head(linear_prediction)
+
+write.csv(linear_prediction, 'DTprediction')
